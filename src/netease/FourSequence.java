a9f899fc4e82d584e8a6f80d4be2389ab572a8a0
Larry <554538252@qq.com>
2017-09-09 17:20:58 +0800

    网易校招题

---
+++ src/netease/FourSequence.java
@@ -0,0 +1,61 @@
+package netease;
+
+import java.util.Scanner;
+
+/**
+ * Created by Larry 09/09/2017
+ */
+public class FourSequence {
+    public final static String YES= "Yes";
+    public final static String NO = "No";
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while (s.hasNext()) {
+            int t = s.nextInt();
+            String[] result = new String[t];
+            for (int i = 0; i < t; i++) {
+                int n = s.nextInt();
+                int[] seq = new int[n];
+                for (int j = 0; j < n; j++) {
+                    seq[j] = s.nextInt();
+                }
+                result[i] = isValid(seq);
+            }
+            for (String str: result) {
+                System.out.println(str);
+            }
+        }
+    }
+    public static String isValid(int[] seq) {
+        String reply = YES;
+        if (seq.length == 1) {
+            if (seq[0]%4 != 0) {
+                reply = NO;
+            }
+            return reply;
+        }
+        int num = 0;
+        int num_4 = 0;
+        for (int i : seq) {
+            if (i%2 != 0) {
+                num++;
+            } else {
+                if (i%4 == 0) {
+                   num_4 ++;
+                }
+            }
+
+        }
+        int num_2 = seq.length-num-num_4;
+        if (num_2>0) {
+            if (num_4 < num ) {
+                reply = NO;
+            }
+        } else {
+            if (num_4 < num-1) {
+                reply = NO;
+            }
+        }
+        return reply;
+    }
+}
---
+++ src/netease/KStr.java
@@ -0,0 +1,50 @@
+package netease;
+
+import java.util.*;
+
+/**
+ * Created by Larry 09/09/2017
+ */
+public class KStr {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while(s.hasNext()) {
+            String str = s.nextLine();
+            char[] strArray = str.toCharArray();
+            Set map = new HashSet();
+            map.add(str);
+            char per = ' ';
+            for (int i = 0; i< strArray.length-1; i++) {
+                if (per == strArray[i]) {
+                    continue;
+                }
+                for (int j =1 ; j < strArray.length; j++) {
+                    if (strArray[j] != strArray[i]) {
+                        String strTmp = insert(strArray, i, j);
+                        if (!map.contains(strTmp)) {
+                            map.add(strTmp);
+                        }
+                    }
+                }
+            }
+            System.out.println(map.size()-1);
+        }
+    }
+    public static String insert(char[] A, int start, int end) {
+        char[] B = new char[A.length];
+        int index = 0;
+        for(int j = 0; j<A.length; j++) {
+            if (j == start) {
+                continue;
+            } else if (j == end) {
+                B[index] = A[start];
+                index++;
+                B[index] = A[j];
+            } else {
+                B[index] = A[j];
+            }
+            index++;
+        }
+        return String.valueOf(B);
+    }
+}
